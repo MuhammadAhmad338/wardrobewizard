8eb977d8faca175f1ab7fe11e7e59217de1d26c6
Muhammad Ahmad <ahmadmuhammad.7700@gmail.com>
2024-06-23 01:14:51 -0700

    latest commits

--- Controllers/contactController.js
+++ Controllers/contactController.js
@@ -22,8 +22,18 @@ const submitContactForm = async (req, res) => {
   }
 };
 
+const getAllContacts = async (req, res) => {
+  try {
+      const contacts = await Contact.find();
+      res.status(200).json(contacts);
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
-  submitContactForm
+  submitContactForm,
+  getAllContacts
 };
 
 
--- Routes/contactRoute.js
+++ Routes/contactRoute.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const {
-    submitContactForm
+    submitContactForm,
+    getAllContacts
 } = require('../Controllers/contactController');
 
 const contactRoute = express.Router();
 
 // Create a new form
 contactRoute.post('/contact', submitContactForm);
+contactRoute.get('/contact/getAllContacts', getAllContacts);
 
 module.exports = contactRoute;
